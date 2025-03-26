808bb23e892d4fc99b274d4a236075807da9b1cf
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-27 02:47:20 +0800

    Prints output with prefix removed

--- removeprefix_alternative.py
+++ removeprefix_alternative.py
@@ -4,4 +4,8 @@ user_input = input("Please enter an input: ")
 prefix = input("Enter a prefix to remove: ")
 
 # remove prefix
+if user_input.startswith(prefix):
+    new_user_input = user_input[len(prefix):]
+
 # print result
+print(new_user_input)
