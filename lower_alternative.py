5836f164bea371ece18c200ab4971bb3f8965f02
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-27 03:04:21 +0800

    lower_alternative.py

--- lower_alternative.py
+++ lower_alternative.py
@@ -1,4 +1,10 @@
 # ask user to input a statement
 user_input = input("Please enter a statement: ")
 # convert each character from string to lowercase
+lowercase_user_input = ""
+for char in user_input:
+    if 'A' <= char <= 'Z':
+        lowercase_user_input += chr(ord(char) + 32)
+    else:
+        lowercase_user_input += char
 # print output
