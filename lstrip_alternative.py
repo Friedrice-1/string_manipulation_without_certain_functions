e48e130c875050b27cfb4af3784d8a1317cab72f
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-27 02:33:22 +0800

    Pseudocode

--- lstrip_alternative.py
+++ lstrip_alternative.py
@@ -1,4 +1,7 @@
 # get input from user
+
+user_input = input("Please enter a statement: ")
+
 # find the first non space character
 # create the new string without the spaces at the left side
 # print output
