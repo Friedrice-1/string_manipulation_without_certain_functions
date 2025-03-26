bab7fcb4f454e18634c062eaddb50da20289690b
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-27 02:40:15 +0800

    lstrip alternative function

--- lstrip_alternative.py
+++ lstrip_alternative.py
@@ -3,5 +3,11 @@
 user_input = input("Please enter a statement: ")
 
 # find the first non space character
+index = 0
+while index < len(user_input) and user_input[index] == ' ':
+    index += 1
+    
+user_input = user_input[index:]
+
 # create the new string without the spaces at the left side
 # print output
