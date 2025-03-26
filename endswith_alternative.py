97b387e7c678502ce85f478e9526e54fb38cce74
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-27 03:22:42 +0800

    User inputs

--- endswith_alternative.py
+++ endswith_alternative.py
@@ -1,4 +1,6 @@
 # ask user to input a statement
+user_input = input("Please enter an input: ")
 # ask user to check what the statement ends with
+ends_with = input("Enter to check if the statements ends with it: ")
 # checks if it ends with the input entered
 # print output
