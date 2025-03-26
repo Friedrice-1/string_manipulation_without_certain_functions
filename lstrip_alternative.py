f55742d6a0cb4677d7744177d12e6414cf891b63
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-27 02:41:09 +0800

    Prints output

--- lstrip_alternative.py
+++ lstrip_alternative.py
@@ -6,8 +6,8 @@ user_input = input("Please enter a statement: ")
 index = 0
 while index < len(user_input) and user_input[index] == ' ':
     index += 1
-    
-user_input = user_input[index:]
 
 # create the new string without the spaces at the left side
+user_input = user_input[index:]
 # print output
+print(user_input)
