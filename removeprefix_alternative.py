e0429e01812f463e68ec3ad5502f9f6b06f5ca19
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-27 02:46:33 +0800

    User inputs

--- removeprefix_alternative.py
+++ removeprefix_alternative.py
@@ -1,4 +1,7 @@
 # ask user to input a word
+user_input = input("Please enter an input: ")
 # ask user the prefix to remove
+prefix = input("Enter a prefix to remove: ")
+
 # remove prefix
 # print result
