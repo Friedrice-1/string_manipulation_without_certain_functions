ba7584d5d3b756f16026885df9630e68a6284a05
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-27 02:57:05 +0800

    Prints output after checking user input

--- isupper_alternative.py
+++ isupper_alternative.py
@@ -2,3 +2,7 @@
 user_input = input("Please enter a statement: ")
 # checks statement if it is all in uppercase
 # prints result
+if user_input.upper().lstrip() == user_input.lstrip():
+    print("Characters are in uppercase")
+else:
+    print("Characters are not in uppercase")
