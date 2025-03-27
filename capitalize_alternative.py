2acb8319811ab61c06a55dcbd55e6cb3d4bf83ba
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-27 18:10:17 +0800

    Converts string to lower case

--- capitalize_alternative.py
+++ capitalize_alternative.py
@@ -1,5 +1,10 @@
 # ask user to input a statement
 user_input = input("Please enter a statement: ")
 # convert statement to lower case
+for char in user_input:
+    if 'a' <= char <= 'z':
+        capitalize_user_input += chr(ord(char) + 32)
+    else:
+        capitalize_user_input += char
 # capitalize the first letter
 # print output
