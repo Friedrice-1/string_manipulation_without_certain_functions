31d8a92ca43e55f71d52519db16e0a0f4b9dbc74
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-27 17:47:09 +0800

    User inputs

--- center_alternative.py
+++ center_alternative.py
@@ -1,4 +1,6 @@
 # ask user to enter a statement
+user_input = input("Enter a statement: ")
 # ask user to enter the width to adjust
+width = int(input("Enter the total width: "))
 # adjust the statement based on the width entered on both sides
 # print output
