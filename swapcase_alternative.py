26e30940e08ee246b28a3507c21bec670499b5cb
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-27 17:55:23 +0800

    Prints formatted string without swapcase function

--- swapcase_alternative.py
+++ swapcase_alternative.py
@@ -1,4 +1,13 @@
 # ask user to input a statement
 user_input = input("Please enter a statement: ")
 # format the statement without the use of swapcase
+swapcase_user_input = ""
+for char in user_input:
+    if 'A' <= char <= 'Z':
+        swapcase_user_input += chr(ord(char) + 32)
+    elif 'a' <= char <= 'z':
+        swapcase_user_input += chr(ord(char) - 32)
+    else:
+        swapcase_user_input += char
 # print output
+print(swapcase_user_input)
