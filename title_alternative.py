8d6e3f29926976f24798aa1cec6cab297da71216
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-27 18:22:42 +0800

    Checks if it is a new word

--- title_alternative.py
+++ title_alternative.py
@@ -1,9 +1,15 @@
 # ask user to input a statement
 user_input = input("Please enter a statement: ")
 # check if characters are in alphabet
+title_case_user_input = ""
+new_word = True
 for char in user_input:
     if char.isalpha():
-        print("Character is in alphabet")
 # check if it is a new word
+        if new_word:
+            print("its a new word")
+        new_word = False
+    else:
+        new_word = True
 # uppercase the first letter and lowercase letters after
 # print output
