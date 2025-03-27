7b6cdfb39d0ade61e1234cab31164232f043eceb
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-27 18:25:12 +0800

    Prints output after formatting without the use of title function

--- title_alternative.py
+++ title_alternative.py
@@ -6,10 +6,14 @@ new_word = True
 for char in user_input:
     if char.isalpha():
 # check if it is a new word
+# uppercase the first letter and lowercase letters after
         if new_word:
-            print("its a new word")
+            title_case_user_input += chr(ord(char) - 32) if 'a' <= char <= 'z' else char
+        else:
+            title_case_user_input += chr(ord(char) + 32) if 'A' <= char <= 'Z' else char
         new_word = False
     else:
+        title_case_user_input += char
         new_word = True
-# uppercase the first letter and lowercase letters after
 # print output
+print(title_case_user_input)
