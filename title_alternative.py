e62858995f272ad101af1a07c2fec7b65d5292ef
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-27 18:20:13 +0800

    Checks if characters are in alphabet

--- title_alternative.py
+++ title_alternative.py
@@ -1,6 +1,9 @@
 # ask user to input a statement
 user_input = input("Please enter a statement: ")
 # check if characters are in alphabet
+for char in user_input:
+    if char.isalpha():
+        print("Character is in alphabet")
 # check if it is a new word
 # uppercase the first letter and lowercase letters after
 # print output
