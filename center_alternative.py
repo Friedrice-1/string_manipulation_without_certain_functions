4b42c6d4b8a5b0436712765fc9f416fa6975ee4f
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-27 17:48:39 +0800

    Prints output after formatting the string

--- center_alternative.py
+++ center_alternative.py
@@ -3,4 +3,10 @@ user_input = input("Enter a statement: ")
 # ask user to enter the width to adjust
 width = int(input("Enter the total width: "))
 # adjust the statement based on the width entered on both sides
+if len(user_input) < width:
+    total_spaces = width - len(user_input)
+    left_spaces = total_spaces // 2
+    right_spaces = total_spaces - left_spaces
+    user_input = " " * left_spaces + user_input + " " * right_spaces
 # print output
+print(f"'{user_input}'")
